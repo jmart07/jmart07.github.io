3d01cb06500ec62650957dc33104535104d3ccae
John Martin <jmart07@gmail.com>
2019-10-21 21:15:03 -0400

    using console to start game logic

--- gnome-warlock/app.js
+++ gnome-warlock/app.js
@@ -7,36 +7,45 @@ $(() => {
     // }
 
 
+    // let side = 5;
+    // console.log(`side: ${side}`);
+    // let startTile = Math.ceil(side/2);
+    // console.log(`origin: ${startTile}`);
 
+    // const $container = $('#container');
+    // for(let i = 1; i <= side; i++) {
+    //     for(let j = 1; j <= side; j++) {
+    //         $tile = $('<div>').addClass('tile');
+    //         $tile.addClass(`row-${i}`);
+    //         $tile.addClass(`column-${j}`);
+    //         $tile.appendTo($container);
+    //     }
+    // }
 
-
-
+    // $gnome = $('<div>').attr('id','gnome');
+    // $(`.row-${startTile}.column-${startTile}`).replaceWith($gnome);
 
 
 
     let side = 5;
+    console.log(`side: ${side}`);
 
-    const $container = $('#container');
+    let startTile = Math.floor(side/2);
+    console.log(`startTile: ${startTile}`);
+
+    let dungeon = [];
     for(let i = 0; i < side; i++) {
+        dungeon.push([]);
         for(let j = 0; j < side; j++) {
-            $tile = $('<div>').addClass('tile');
-            $tile.addClass(`row-${i}`);
-            $tile.addClass(`column-${j}`);
-            $tile.appendTo($container);
+            // dungeon[i].push(`${i + 1},${j + 1}`)
+            dungeon[i].push('_')
+            // console.log(dungeon[i]);
         }
     }
 
+    let gnome = 'G';
+    dungeon[startTile][startTile] = gnome;
 
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log(dungeon)
+  
 });
