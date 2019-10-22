4b281c48c7952ae83cc791b468924bb49ea3a4f0
John Martin <jmart07@gmail.com>
2019-10-21 20:00:35 -0400

    board made for messing around

--- gnome-warlock/app.js
+++ gnome-warlock/app.js
@@ -1,4 +1,30 @@
 $(() => {
+    // const player = class {
+
+
+
+
+    // }
+
+
+
+
+
+
+
+
+
+    let side = 5;
+
+    const $container = $('#container');
+    for(let i = 0; i < side; i++) {
+        for(let j = 0; j < side; j++) {
+            $tile = $('<div>').addClass('tile');
+            $tile.addClass(`row-${i}`);
+            $tile.addClass(`column-${j}`);
+            $tile.appendTo($container);
+        }
+    }
 
 
 
