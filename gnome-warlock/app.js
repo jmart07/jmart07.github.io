beeabbe0ec152bf02e7a378fd63e94ee53c90b50
John Martin <jmart07@gmail.com>
2019-10-21 21:59:34 -0400

    i can move the gnome around in my console

--- gnome-warlock/app.js
+++ gnome-warlock/app.js
@@ -12,7 +12,7 @@ $(() => {
     // let startTile = Math.ceil(side/2);
     // console.log(`origin: ${startTile}`);
 
-    // const $container = $('#container');
+    const $container = $('#container');
     // for(let i = 1; i <= side; i++) {
     //     for(let j = 1; j <= side; j++) {
     //         $tile = $('<div>').addClass('tile');
@@ -34,18 +34,64 @@ $(() => {
     console.log(`startTile: ${startTile}`);
 
     let dungeon = [];
-    for(let i = 0; i < side; i++) {
+    for(let row = 0; row < side; row++) {
         dungeon.push([]);
-        for(let j = 0; j < side; j++) {
-            // dungeon[i].push(`${i + 1},${j + 1}`)
-            dungeon[i].push('_')
-            // console.log(dungeon[i]);
+        for(let column = 0; column < side; column++) {
+            dungeon[row].push('_')
+            // dungeon[row].push(`${row},${column}`)
         }
     }
 
-    let gnome = 'G';
-    dungeon[startTile][startTile] = gnome;
+    // console.log(dungeon)
 
-    console.log(dungeon)
-  
+    let gnome = 'G';
+    console.log(`gnome: ${gnome}`);
+
+    let gnomeRow = startTile;
+    let gnomeColumn = startTile;
+    console.log(`gnome location: ${gnomeRow},${gnomeColumn}`);
+
+    dungeon[gnomeRow][gnomeColumn] = gnome;
+
+    // console.log(dungeon);
+
+    for(let i = 0; i < 3; i++) {
+
+        let move = prompt('move');
+        console.log(`move: ${move}`)
+
+        let gnomeRowPrevious = gnomeRow
+        let gnomeColumnPrevious = gnomeColumn
+
+        if(move === 'n') {
+            gnomeRow--;
+        } else if(move === 'e') {
+            gnomeColumn++;
+        } else if(move === 's') {
+            gnomeRow++;
+        } else if(move === 'w') {
+            gnomeColumn--;
+        } else {
+            console.log('bad input');
+        }
+
+        dungeon[gnomeRow][gnomeColumn] = gnome;
+
+        dungeon[gnomeRowPrevious][gnomeColumnPrevious] = 'p'
+
+        console.log(`gnome location: ${gnomeRow},${gnomeColumn}`);
+
+        console.log(dungeon);
+
+        for(let x = 0; x < side; x++) {
+            for(let y = 0; y < side; y++) {
+                const $tile = $('<div>').attr('id',`${x},${y}`)
+                $tile.text(`row${x},column${y}`);
+                $tile.addClass('tile');
+                $container.append($tile);
+            }
+        }
+
+    }
+    console.log('end');
 });
